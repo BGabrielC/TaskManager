9cc8251e67b5b244c7a0171364b3b55428524f65
Gabriel Cutas <bcutas12@gmail.com>
2023-06-14 23:07:33 +0200

    nueva clase Task y comienzo del menu de inicio

--- TaskApp/Program.cs
+++ TaskApp/Program.cs
@@ -3,13 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskManagerLib;
+using Task = TaskManagerLib.Task;
 
-namespace TaskApp
+namespace TaskManager
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            //List<Task> tasks = Task.ReadFromFile("tasks.txt");
+            bool exit = false;
+            while (!exit)
+            {
+                Console.WriteLine("1. View all tasks");
+                Console.WriteLine("2. Add a task");
+                Console.WriteLine("3. Exit");
+                Console.Write("Choose an option: ");
+                string option = Console.ReadLine();
+                switch (option)
+                {
+                    case "1":
+                        //ViewAllTasks(tasks);
+                        break;
+                    case "2":
+                        //AddTask(tasks);
+                        break;
+                    case "3":
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option");
+                        break;
+                }
+            }
+
         }
     }
 }
---
+++ TaskManagerLib/Task.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagerLib
+{
+    public class Task
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool Completed { get; set; }
+ 
+        public Task(string name, string description, DateTime dueDate)
+        {
+            Name = name;
+            Description = description;
+            DueDate = dueDate;
+            Completed = false;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} - {1} - {2} - {3}", Name, Description, DueDate, Completed);
+        }
+
+
+        public static Task ParseFromFile(string line)
+        {
+            string[] parts = line.Split('|');
+            string name = parts[0];
+            string description = parts[1];
+            DateTime dueDate = DateTime.Parse(parts[2]);
+            bool completed = bool.Parse(parts[3]);
+            return new Task(name, description, dueDate) { Completed = completed };
+        }
+
+        public static void SaveToFile(string fileName, Task task)
+        {
+            string line = string.Format("{0}|{1}|{2}|{3}", task.Name, task.Description, task.DueDate, task.Completed);
+            System.IO.File.AppendAllText(fileName, line + Environment.NewLine);
+        }
+
+       
+        public static List<Task> ReadFromFile(string fileName)
+        {
+            List<Task> tasks = new List<Task>();
+            string[] lines = System.IO.File.ReadAllLines(fileName);
+            foreach (string line in lines)
+            {
+                tasks.Add(ParseFromFile(line));
+            }
+            return tasks;
+        }
+
+        public static Task ParseFromConsole()
+        {
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Description: ");
+            string description = Console.ReadLine();
+            Console.Write("Due Date: ");
+            DateTime dueDate = DateTime.Parse(Console.ReadLine());
+            return new Task(name, description, dueDate);
+        }
+
+    }
+}
